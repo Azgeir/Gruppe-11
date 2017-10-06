5eec1dee836b1fc9c75cf747f89264a70e6bcd89
HCHB <HCHB@DESKTOP-GH3BBC7>
2017-10-06 09:33:53 +0200

    test of github commit and push

---
+++ OOP_Semester_Project/src/worldofzuul/MainCharacter1.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldofzuul;
+
+/**
+ *
+ * @author HCHB
+ */
+public class MainCharacter1 {
+
+    private Inventory inventory1;
+    
+    public MainCharacter1(){
+
+        this.inventory1 = new Inventory(5);
+    }
+
+    /*public int[] getInventory() {
+        return inventory;
+    }
+    */
+
+    public void setInventory(int i, int n) {
+        
+        int sum = 0;
+                
+        for (int j = 0 ; j < this.inventory.length ; j++){
+            sum += this.inventory[j];
+        }
+        
+        if(sum+n<=4){
+            this.inventory[i] += n;
+        }
+        else{
+            System.out.println("You can't carry this much");
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
