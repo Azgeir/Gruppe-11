41eaaa57f4ce444856ee20502f94aac9d833988d
HCHB <HCHB@DESKTOP-GH3BBC7>
2017-10-06 11:54:59 +0200

    Merge origin/master

---
+++ Semester Project/src/worldofzuul/Starter.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldofzuul;
+
+/**
+ *
+ * @author Aske Wulf
+ */
+// This class starts the game by creating an instance of the Game class.
+public class Starter {
+    public static void main(String[] args){
+        Game start = new Game();
+        start.play();
+        System.out.println("test HCHB");
+    }
+    
+}
