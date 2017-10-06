72f18cf0e38950b45bf9afe3c24a018f18e3fde9
HCHB <HCHB@DESKTOP-GH3BBC7>
2017-10-06 10:33:53 +0200

    test of github commit and push

---
+++ OOP Semester Project/src/worldofzuul/Start.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldofzuul;
+
+/**
+ *
+ * @author HCHB
+ */
+public class Start {
+    public static void main(String[] args){
+        
+        Game testGame = new Game();
+        
+        testGame.play();
+        System.out.println("this is a test");
+        System.out.println("this is also a test");
+        
+    }
+    
+}
