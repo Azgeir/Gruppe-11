65b99403b1bb6cf6bde327932321ddaec16ebca1
laurabj <32192500+laurabj@users.noreply.github.com>
2017-10-12 14:45:34 +0200

    Commit

---
+++ worldofzuul/src/worldofzuul/MedKit.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldofzuul;
+
+/**
+ *
+ * @author laurabrinkholmjustesen
+ */
+
+// This class represents a MedKit item.
+public class MedKit extends Item {
+    // Data fields:
+    private int healthAmount; // Health points added to player's health when used
+    
+    // (£)This constructor creates an instance of MedKit with a specified health amount
+    public MedKit(int healthAmount) {
+        this.healthAmount = healthAmount;
+    }
+    
+    // (£)This constructor creates an instance of MedKit with a default health amount
+    public MedKit() {
+        this.healthAmount = 50;
+    }
+    
+    // This method returns the health amount of the MedKit
+    public int getHealthAmount() {
+        return this.healthAmount;
+    }
+}
